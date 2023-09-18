6da7f6de84d319c5e3f62293cfcf43ce0e54453c
amirh-far <Ahosein54@gamil.com>
2023-09-18 16:00:21 +0330

    3 Create your first Api view

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint="http://localhost:8000/"
+endpoint="http://localhost:8000/api"
 
 get_response = requests.get(endpoint) #, json={"query": "Hello world"}) # HTTP request
 print(get_response.text ) # Print raw text response
@@ -11,4 +11,4 @@ print(get_response.status_code)
 # HTTP Request -> HTML
 # REST API Request -> JSON
 # JavaScript Object Notation ~ Python Dict
-# print(get_response.json())
+print(get_response.json()["message"])
