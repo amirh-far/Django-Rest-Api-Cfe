7ce280dadb66d54399e6fcc3d690c2faff0ad127
amirh-far <Ahosein54@gamil.com>
2023-09-18 17:33:55 +0330

    5 Django Model instace as Api response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,24 +1,15 @@
 from django.http import JsonResponse
 import json
 import requests
-
+from products.models import Product
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    # print(dir(request))
-    print(request.GET)
-    body = request.body
-    # print(body) #  byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) 
-    except:
-        pass
-    print(data.keys())
-    data["headers"] = dict(request.headers)
-    print(request.headers)
-    data["content_type"] = request.content_type
-    data["params"] = dict(request.GET)
-
+    if model_data:
+        data["id"] = model_data.id
+        data["title"] = model_data.title
+        data["content"] = model_data.content
+        data["price"] = model_data.price
 
     # return JsonResponse({"message": "Hi there, this is your first django API response!"})
     return JsonResponse(data)
