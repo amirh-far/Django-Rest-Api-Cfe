df82a5dbd17299fbeb2f3a8f64a23b4b4db7e69b
amirh-far <Ahosein54@gamil.com>
2023-09-18 15:47:06 +0330

    2 Run Django Project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,12 +1,14 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint="http://localhost:8000/"
 
-get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP request
-print(get_response.text) # Print raw text response
+get_response = requests.get(endpoint) #, json={"query": "Hello world"}) # HTTP request
+print(get_response.text ) # Print raw text response
+print(get_response.status_code)
 
 # HTTP Request -> HTML
 # REST API Request -> JSON
 # JavaScript Object Notation ~ Python Dict
-print(get_response.json())
+# print(get_response.json())
