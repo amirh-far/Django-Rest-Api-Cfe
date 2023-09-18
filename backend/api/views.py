c29c788d8cb468ad9ca144af429672e030022886
amirh-far <Ahosein54@gamil.com>
2023-09-18 22:29:10 +0330

    8 Ingest Data with Django Rest Framework Views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,28 +1,22 @@
-# from django.http import JsonResponse
+from django.http import JsonResponse
 import json
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.forms.models import model_to_dict
+# from django.forms.models import model_to_dict
 from products.models import Product
 from products.serializers import ProductSerializer
 
-@api_view(["GET", "POST"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
     """
     DRF Api View
     """
-    # if request.method is not "POST":
-    #     return Response({"detail": "GET is not allowed"}, status=405) 
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data = model_to_dict(instance, fields=["id", "title", "price"])
-        data = ProductSerializer(instance).data
 
-
-    return Response(data)
-
-
-
+    serializer = ProductSerializer(data=request.data)    
+    if serializer.is_valid(raise_exception=True):
+        instance = serializer.save()
+        # Just like this code : instance = form.save()
+        print(instance)
+        return Response(serializer.data)
 
     # return JsonResponse({"message": "Hi there, this is your first django API response!"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint="http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"}) # HTTP request
+get_response = requests.post(endpoint, params={"abc": 123}, json={"title": "Hello world"}) # HTTP request
 print(get_response.text ) # Print raw text response
 print(get_response.status_code)
 
