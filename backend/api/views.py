37c95307199cd3e2b4ca5212c08d11f5c608a1ae
amirh-far <Ahosein54@gamil.com>
2023-09-18 20:36:40 +0330

    6 Rest Framework and Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,21 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 import json
-import requests
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.forms.models import model_to_dict
 from products.models import Product
+
+
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF Api View
+    """
+    if request.method is not "POST":
+        return Response({"detail": "GET is not allowed"}, status=405) 
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data["id"] = model_data.id
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
-
+        data = model_to_dict(model_data, fields=["id", "title", "price"])
     # return JsonResponse({"message": "Hi there, this is your first django API response!"})
-    return JsonResponse(data)
+    return Response(data)
