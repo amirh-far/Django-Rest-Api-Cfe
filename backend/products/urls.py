f37b04a0ae5122dc8cc9dc14b8080c68d06c01f5
amirh-far <Ahosein54@gamil.com>
2023-09-19 19:19:51 +0330

    14 Session Authentication & Permissions

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ProductMixinView.as_view()),
-    path("<int:pk>/update", views.ProductMixinView.as_view()), 
-    path("<int:pk>/delete", views.ProductMixinView.as_view()), 
-    path("<int:pk>/", views.ProductMixinView.as_view()),
+    path("", views.ProductListCreateAPIView.as_view()),
+    path("<int:pk>/update", views.ProductUpdateAPIView.as_view()), 
+    path("<int:pk>/delete", views.ProductDeleteAPIView.as_view()), 
+    path("<int:pk>/", views.ProductListCreateAPIView.as_view()),
  
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, permissions, authentication
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 
@@ -10,6 +10,8 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         title = serializer.validated_data.get("title")
