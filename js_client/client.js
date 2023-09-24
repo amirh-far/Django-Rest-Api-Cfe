6c48570f0434debbac2b49d3368569a4a1dcc80f
amirh-far <Ahosein54@gamil.com>
2023-09-24 23:22:03 +0330

    33 Search via REST Api & JS Client

--- js_client/client.js
+++ js_client/client.js
@@ -1,8 +1,15 @@
 const contentContainer = document.getElementById("content-container")
 const loginForm = document.getElementById("login-form")
+const searchForm = document.getElementById("search-form")
+
 const baseEndpoint = "http://localhost:8000/api"
+
 if (loginForm) {
     loginForm.addEventListener("submit", handleLogin)
+}
+
+if (searchForm) {
+    loginForm.addEventListener("submit", handleSearch)
 }
 
 function handleLogin(event) {
@@ -34,16 +41,60 @@ function handleLogin(event) {
 
 }   
 
+function handleSearch(event) {
+    event.preventDefault()
+    let formData = new FormData(searchForm)
+    let data = Object.fromEntries(formData)
+    let searchParams = new URLSearchParams(data)
+    const endpoint = `${baseEndpoint}/search/?${searchParams}`
+    const headers = {
+        "Content-Type": "application/json",
+    }
+    const authToken = localStorage.getItem('access') 
+    if (authToken) {
+        headers['Authorization'] = `Bearer ${authToken}`
+    }
+    const options = {
+        method: "GET",
+        headers: headers
+    }
+    fetch(endpoint, options) //  Promise
+    .then(response=>{
+        return response.json()
+    })
+    .then(data => {
+        const validData = isTokenNotValid(data)
+        if (validData && contentContainer){
+            contentContainer.innerHTML = ""
+            if (data && data.hits) {
+                let htmlStr  = ""
+                for (let result of data.hits) {
+                    htmlStr += "<li>"+ result.title + "</li>"
+                }
+                contentContainer.innerHTML = htmlStr
+                if (data.hits.length === 0) {
+                    contentContainer.innerHTML = "<p>No results found</p>"
+                }
+            } else {
+                contentContainer.innerHTML = "<p>No results found</p>"
+            }
+        }
+    })
+    .catch(err=> {
+        console.log('err', err)
+    })
+}
+
 function handleAuthData(authData, callback) {
     localStorage.setItem("access", authData.access) // setItem("key", "value")
     localStorage.setItem("refresh", authData.refresh) // setItem("key", "value")
     if (callback) {
-         callback()
+        callback()
     }
     
 }
 
-function writeToContent(data) {
+function writeToContainer(data) {
     if (contentContainer) {
         contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4) + "</pre>"
     }
@@ -100,8 +151,7 @@ function getProductList() {
         const validData = isTokenNotValid(data)
         console.log(data)
         if (validData) {
-        writeToContent(data)
-
+        writeToContainer(data)
         }
     })
 }
